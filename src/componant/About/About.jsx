5d716ebb764c62050578dedf25559ca00643dc4f
Amjad-addas <spector.mj8@gmail.com>
2023-08-02 13:34:45 +0300

    first commit

--- src/componant/About/About.jsx
+++ src/componant/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
    <div className="about">
     <h1 className='about-title'>The Store Was Build Complitly By <span className='my-name'> Amjad Addas</span></h1>
-    <a href="/store" className="stor">Go To Store And Start Shopping</a>
+    <a href="#/store" className="stor">Go To Store And Start Shopping</a>
    </div>
   )
 }
--- src/componant/Home/Home.jsx
+++ src/componant/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="home">
       <h2 className="title">The Store is Almost Done</h2>
-      <a href="/store" className="stor">Go To Store And Start Shopping</a>
+      <a href="#/store" className="stor">Go To Store And Start Shopping</a>
     </div>
   )
 }
